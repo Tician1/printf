2246c4457e6a2156af9556f315e79d73987783d3
Emem Udofia <ememudofia456@gmail.com>
2022-07-12 01:22:17 -0700

    collaboration

--- _printf.c
+++ _printf.c
@@ -1,37 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
- */
+  *_printf - prints formatted output.
+  *@format: input.
+  *
+  *Return: number of chars printed or -1.
+  */
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	va_list args;
+	int i, len;
+	int (*get_ptr)(va_list, int);
 
-	if (format == NULL)
+	va_start(args, format);
+	if (!(format))
 		return (-1);
-
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	i = 0;
+	len = 0;
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				len += _putchar(format[i]);
+				i++;
+				continue;
+			}
+			if (format[i] == '\0')
+				return (-1);
+			get_ptr = get_print_func(format[i]);
+			if (get_ptr != NULL)
+				len = get_ptr(args, len);
+			else
+			{
+				len += _putchar(format[i - 1]);
+				len += _putchar(format[i]);
+			}
+			i++;
+		}
+		else
+		{
+			len += _putchar(format[i]);
+			i++;
+		}
+	}
+	va_end(args);
+	return (len);
 }
